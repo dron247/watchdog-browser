f2996c18c524c41ba815b29a0ea91ded7d200068
dron247 <dron247@DESKTOP-ET5ESDB>
2016-10-24 21:24:12 +0800

    Вывод заголовка

---
+++ WatchdogBrowser/WatchdogBrowser/Handlers/WatchdogLifespanHandler.cs
@@ -0,0 +1,32 @@
+﻿using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WatchdogBrowser.CustomEventArgs;
+
+namespace WatchdogBrowser.Handlers {
+    public class WatchdogLifespanHandler : ILifeSpanHandler {
+
+        public event EventHandler<TabRequestEventArgs> NewTabRequest;
+
+        public bool DoClose(IWebBrowser browserControl, IBrowser browser) {
+            return false;
+        }
+
+        public void OnAfterCreated(IWebBrowser browserControl, IBrowser browser) {
+        }
+
+        public void OnBeforeClose(IWebBrowser browserControl, IBrowser browser) {
+        }
+
+        public bool OnBeforePopup(IWebBrowser browserControl, IBrowser browser, IFrame frame, string targetUrl, string targetFrameName, WindowOpenDisposition targetDisposition, bool userGesture, IPopupFeatures popupFeatures, IWindowInfo windowInfo, IBrowserSettings browserSettings, ref bool noJavascriptAccess, out IWebBrowser newBrowser) {
+            //return true;
+            //return parent.OnBeforePopup(browserControl, browser, frame, targetUrl, targetFrameName, targetDisposition, userGesture, popupFeatures, windowInfo, browserSettings, ref noJavascriptAccess, out newBrowser);
+            newBrowser = null;
+            NewTabRequest?.Invoke(this, new TabRequestEventArgs { URL = targetUrl });
+            return true;
+        }
+    }
+}
---
+++ WatchdogBrowser/WatchdogBrowser/Handlers/WatchdogMenuHandler.cs
@@ -0,0 +1,26 @@
+﻿using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchdogBrowser.Handlers {
+    public class WatchdogMenuHandler : IContextMenuHandler {
+        void IContextMenuHandler.OnBeforeContextMenu(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model) {
+            model.Clear();
+        }
+
+        bool IContextMenuHandler.OnContextMenuCommand(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, CefMenuCommand commandId, CefEventFlags eventFlags) {
+            return false;
+        }
+
+        void IContextMenuHandler.OnContextMenuDismissed(IWebBrowser browserControl, IBrowser browser, IFrame frame) {
+            //throw new NotImplementedException();
+        }
+
+        bool IContextMenuHandler.RunContextMenu(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model, IRunContextMenuCallback callback) {
+            return false;
+        }
+    }
+}
